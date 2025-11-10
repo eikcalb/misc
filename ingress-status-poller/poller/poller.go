7f2481be9c34ade4bc4742dae84c3e5ebdc19aac
Onome Agwa <onome.agwa.ext@bbc.co.uk>
2025-11-10 13:08:23 +0000

    - Fix timer.

--- ingress-status-poller/poller/poller.go
+++ ingress-status-poller/poller/poller.go
@@ -41,7 +41,7 @@ type PollerConfig struct {
 
 func getConfig() (c *PollerConfig, err error) {
 	fs := flag.NewFlagSet("poller", flag.ExitOnError)
-	interval := fs.Duration("d", time.Duration(5), "Polling interval duration")
+	interval := fs.Duration("d", time.Duration(5)*time.Second, "Polling interval duration")
 	environment := fs.String("e", "", "Ingress endpoint environment")
 
 	err = fs.Parse(os.Args[1:])
